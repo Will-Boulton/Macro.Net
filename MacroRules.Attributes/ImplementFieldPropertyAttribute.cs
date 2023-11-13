01d5e33f63921ce5bed4d035890d33985a28b4e1
WilliamBoulton <will@willboulton.com>
2023-11-13 21:01:42 +0000

    wip

---
+++ MacroRules.Attributes/ImplementFieldPropertyAttribute.cs
@@ -0,0 +1,16 @@
+﻿namespace MacroRules.Attributes;
+
+[assembly:Macro("","")]
+
+
+[AttributeUsage(AttributeTargets.Assembly)]
+public class MacroAttribute(string macroName, string rules) : Attribute
+{
+    
+}
+
+[AttributeUsage(AttributeTargets.Method)]
+public class MacroMethodAttribute(string macroName)
+{
+    
+}
---
+++ MacroRules.SourceGenerator/SourceGenerationHelper.cs
@@ -0,0 +1,14 @@
+﻿namespace MacroRules.SourceGenerator;
+
+public class SourceGenerationHelper
+{
+    public const string MacroRulesTemplate = 
+        """
+        namespace MacroRules;
+        
+        internal partial class macro_rules
+        {
+        
+        }                                     
+        """;
+}
